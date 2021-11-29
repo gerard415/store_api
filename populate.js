29dc1c1bf9b287044237ef17898be55d8f63e876
Chinedum <chinedumike85@gmail.com>
2021-11-29 18:50:09 +0100

    deleting the former data from the database and adding our own from products.json

--- populate.js
+++ populate.js
@@ -9,9 +9,13 @@ const productModel = require('./models/product')
 const start = async ()=>{
     try{
         await connectDB(process.env.MONGO_URI)
+        await productModel.deleteMany() //deletes the current items in your database
+        await productModel.create(productJson) //adds all the items inside the json file
         console.log('success')
+        process.exit(0) //exiting but was succesfull
     }catch(err){
         console.log(err)
+        process.exit(1) //exiting butfor an error
     }
 }
 start()
