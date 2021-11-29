cce501c0b5abdc5c756cdf7b22bc143a87f9e9ea
Chinedum <chinedumike85@gmail.com>
2021-11-29 22:04:28 +0100

    sorting items commit

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,13 @@
 const productModel = require('../models/product')
 
 const getAllproductsStatic = async (req, res)=>{
-    const products = await productModel.find({ name: "vase table"})
+    const products = await productModel.find({}).sort('-name price') //leave a space to sort using multiple items, a minus sign to start the sort from the back
     res.status(200).json({products, nbHits: products.length})
 
 }
 
 const getAllproducts = async (req, res)=>{
-    const { featured, company, name } = req.query;
+    const { featured, company, name, sort } = req.query;
     const queryObject = {} //creating a new object
 
     if (featured) {
@@ -20,7 +20,17 @@ const getAllproducts = async (req, res)=>{
         queryObject.name = { $regex: name, $options: 'i' }; //search using the pattern of the name and making sure its not case sensitive
     }       
 
-    const products = await productModel.find(queryObject)
+    // find the queryObject and equate it to result, if sort is provided then sort 
+    let result = productModel.find(queryObject)
+    if(sort){
+        const sortList = sort.split(',').join('')
+        result = result.sort(sortList)
+    }
+    else{
+        result = result.sort('createAt')
+    }
+
+    const products = await result
     res.status(200).json({products, nbHits: products.length} )
 
 }
