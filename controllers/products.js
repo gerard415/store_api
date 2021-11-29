93264f04642b64cb8814162d220347f6f0d5f5c8
Chinedum <chinedumike85@gmail.com>
2021-11-29 19:26:41 +0100

    filtering the json data commit 1

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,14 @@
+const productModel = require('../models/product')
+
 const getAllproductsStatic = async (req, res)=>{
-    throw new Error('testing async errors')
-    res.status(200).json({msg:"get all products static"})
+    const products = await productModel.find({ name: "vase table"})
+    res.status(200).json({products, nbHits: products.length})
 
 }
 
 const getAllproducts = async (req, res)=>{
-    throw new Error('testing async errors')
-    res.status(200).json({msg:"get all products"})
+    const products = await productModel.find(req.query)
+    res.status(200).json({products, nbHits: products.length} )
 
 }
 
