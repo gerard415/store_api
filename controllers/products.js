0a0f8b9245a16d687f3a7f162a76cfd88bce4f80
Chinedum <chinedumike85@gmail.com>
2021-11-29 20:48:20 +0100

    filtering the json data commit 2:   setting up the query object for featured, company and name

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,20 @@ const getAllproductsStatic = async (req, res)=>{
 }
 
 const getAllproducts = async (req, res)=>{
-    const products = await productModel.find(req.query)
+    const { featured, company, name } = req.query;
+    const queryObject = {} //creating a new object
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false; //turning operator which means that if featured is true then equate it to true else equate it to false
+    }
+    if (company) {
+        queryObject.company = company;
+    }
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' }; //search using the pattern of the name and making sure its not case sensitive
+    }       
+
+    const products = await productModel.find(queryObject)
     res.status(200).json({products, nbHits: products.length} )
 
 }
