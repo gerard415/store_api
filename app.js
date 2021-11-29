8630430b984ce76793d3bc2ab46ea9687dc3b04b
Chinedum <chinedumike85@gmail.com>
2021-11-29 16:43:02 +0100

    setting up the async errors, the middleware and the routes

--- app.js
+++ app.js
@@ -3,7 +3,24 @@ const app = express()
 require('dotenv').config()
 const connectDB = require('./db/connect')
 
-port = 5000
+//async errors
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+  });
+
+//async errors
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+
+const port = process.env.PORT || 5000;
 const start = async ()=>{
     try {
         await connectDB(process.env.MONGO_URI)
